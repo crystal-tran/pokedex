10a0b4bd170a9dee13930d3974786030074257cc
Teagan McClintock <teagan.mcclintock@gmail.com>
2024-01-14 17:25:10 -0500

    Added captions for type and EXP, and added styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,14 @@
 /** Shows a single pokemon with their name, image, and type*/
 
+import "./Pokecard.css";
+
 function Pokecard({ name, imageUrl, type, base_experience }) {
   return (
     <div className="Pokecard">
       <h1>{name}</h1>
       <img src={imageUrl} alt={name} />
-      <p>{type}</p>
-      <p>{base_experience}</p>
+      <p>Type: {type}</p>
+      <p>EXP: {base_experience}</p>
     </div>);
 }
 
